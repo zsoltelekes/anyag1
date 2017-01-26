2f45b2718e79f4f9f03bd59a1636cdfd4306ed20
zsoltelekes <e15.elekes.zsolt@palfy-vizugyi.hu>
2017-01-26 19:55:14 +0100

    masodik

---
+++ Desktop/anyag2.py
@@ -0,0 +1,16 @@
+refrén = 'Csillárom, haj!'
+print('Csizmadialegények')
+print('\nCsizmadialegények')
+print(refrén)
+print('Nyolcan ülnek egy lóra')
+print(refrén)
+print('Úgy mennek a jó borra')
+print(refrén)
+# a következő sorban felülírjuk a változó értékét
+refrén = 'Sej, ripityom!'
+print('Parancsolnak kocsmárosnak;')
+print('Mérjen egy decit nyócuknak!')
+print(refrén) # itt már Sej, ripityom! lesz
+print('Azt se tudják kifizetni.')
+print('Az ablakon szekdesnek ki.')
+print(refrén)
